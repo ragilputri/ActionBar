710b633e7b11780639ff04caedef0aaf08303843
ragilputri <3103119151@student.smktelkom-pwt.sch.id>
2020-11-14 13:20:28 -0800

    first commit

---
+++ app/src/main/java/ragilputri/stematel/myactionbar/MenuActivity.kt
@@ -0,0 +1,11 @@
+package ragilputri.stematel.myactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
